07d31ec17ba597c9aa4b86168a6ee4b585f362c8
masayuki201 <masayuki201_201@yahoo.co.jp>
2023-01-23 18:57:10 +0900

    add:View TodoLists

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <input type="text">
+  <button>登録</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TodoInput",
+});
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,40 @@
+<template>
+ <div>
+   <ul class="todolist">
+     <li v-for="todo in state.todoList" :key="todo.todo">{{ todo.todo }}</li>
+   </ul>
+   <TodoInput />
+ </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+import TodoInput from '../components/TodoInput.vue'
+
+export default defineComponent({
+  name: "TodoList",
+  components: {
+    TodoInput,
+  },
+  setup() {
+    const state = reactive<{ todoList: [{todo: String}, {todo: String}, {todo: String}];}>({
+      todoList: [
+        {
+          todo: 'クリーニングへ行く',
+        },
+        {
+          todo: 'スーパーへ行く',
+        },
+        {
+          todo: '銀行へ行く',
+        },
+      ]
+    });
+    return { state };
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
