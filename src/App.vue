7e86cca6454fe0e818d6ca1ba721b94237575a62
masayuki201 <masayuki201_201@yahoo.co.jp>
2023-01-30 09:34:01 +0900

    add:style change

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
+  <div>
+    <h1>
+      TodoList7
+    </h1>
+  </div>
   <router-view />
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <TodoList />
     <TodoInput />
+    <TodoList />
   </div>
 </template>
 
