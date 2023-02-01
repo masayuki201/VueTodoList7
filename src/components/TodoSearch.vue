a171be0dcba85b1515d229031b454f893f0fb0a3
masayuki201 <masayuki201_201@yahoo.co.jp>
2023-02-02 06:46:40 +0900

    wip

---
+++ src/components/TodoSearch.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input type="text" v-model="keyword" placeholder="検索">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from "vue";
+import store from "../store";
+import { useStore } from "vuex";
+
+
+export default defineComponent({
+  name: "TodoSearch",
+  setup(){
+    const keyword = ref<string>('')
+
+    const filteredLists = computed(() => {
+      const searchKeyword = store.state.keyword;
+      if (searchKeyword === '') return store.state.todoList;
+      return store.commit('search', searchKeyword)
+    });
+
+    return { keyword, filteredLists };
+  }
+
+});
+
+
+</script>
+
+<style scoped>
+
+</style>
